e00b4f4ca27f284cbaac14616e261eecc43351bf
Shugo-G <hugo_giovanetti@hotmail.com>
2024-02-29 11:30:01 -0300

    creacion de modelo server

--- src/app_servers/models.py
+++ src/app_servers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Server(models.Model):
+    nombre = models.models.CharField(max_length=30)
+    cpu = models.FloatField()
+    real_memory = models.FloatField()
+    virtual_memory = models.FloatField()
+    disck_space = models.FloatField()
